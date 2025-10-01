8c8ac89aee2ef061d39f769b6f1f9e56a8ca2e3d
CarlFJuanillo <carlodie.juanillo@smc.pshs.edu.ph>
2025-10-01 08:54:44 +0800

    Add files via upload

---
+++ SG6_Code2.py
@@ -0,0 +1,8 @@
+# enter radius of the circle
+radius = float(input("Enter the radius of the circle: "))
+
+# calculate the area of the circle
+area = 3.14159 * radius * radius
+
+# display the area rounded to 2 decimal places
+print("The area of the circle is: {:.2f}".format(area))
